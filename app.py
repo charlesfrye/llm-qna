c201458e5bbe89a0ef750f0b72c1c704fc490169
Charles Frye <cfrye59@gmail.com>
2023-04-20 11:52:39 -0700

    update req'ts of backend app

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ import modal
 
 image = modal.Image.debian_slim(python_version="3.10").pip_install(
     "langchain~=0.0.98",
-    "pypdf~=3.8",
     "openai~=0.26.3",
     "pinecone-client",
-    "pymongo==3.11",
+    "pymongo[srv]==3.11",
     "tiktoken",
 )
 
